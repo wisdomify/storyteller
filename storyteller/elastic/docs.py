2f2a637aa8c16fa54cb0fcae1a9a20498e13d70a
teang1995 <teang1995@ajou.ac.kr>
2021-11-03 22:00:12 +0900

    [#19] fix decorator staticmethod -> classmethod at storyteller/elastic/docs.py

--- storyteller/elastic/docs.py
+++ storyteller/elastic/docs.py
@@ -12,7 +12,7 @@ class Story(Document):
     # --- common fields --- #
     sents = Text(analyzer="nori_analyzer")
 
-    @staticmethod
+    @classmethod
     def stream_from_corpus(cls) -> Generator['Story', None, None]:
         """
         :return: a stream of Stories.
@@ -60,7 +60,7 @@ class GK(Story):
     """
     일반 상식 인덱스
     """
-    @staticmethod
+    @classmethod
     def stream_from_corpus() -> Generator['GK', None, None]:
         corpus_json_path = os.path.join(GK_DIR, "ko_wiki_v1_squad.json")
         with open(corpus_json_path, 'r') as fh:
@@ -83,7 +83,7 @@ class SC(Story):
     profile_id = Keyword()
     talk_id = Keyword()
 
-    @staticmethod
+    @classmethod
     def stream_from_corpus() -> Generator['SC', None, None]:
         train_json_path = os.path.join(SC_DIR, "Training", "감성대화말뭉치(최종데이터)_Training.json")
         val_json_path = os.path.join(SC_DIR, "Validation", "감성대화말뭉치(최종데이터)_Validation.json")
@@ -108,7 +108,7 @@ class MR(Story):
     # --- additional fields for MR --- #
     title = Keyword()
 
-    @staticmethod
+    @classmethod
     def stream_from_corpus() -> Generator['MR', None, None]:
         normal_json_path = os.path.join(MR_DIR, "기계독해분야", "ko_nia_normal_squad_all.json")
         no_answer_json_path = os.path.join(MR_DIR, "기계독해분야", "ko_nia_noanswer_squad_all.json")
@@ -135,6 +135,7 @@ class News(Story):
     provider = Keyword()
     date = Keyword()
 
+    @classmethod
     def stream_from_corpus() -> Generator['News', None, None]:
         news_data_path = os.path.join(NEWS_DIR, 'news_data.json')
 
