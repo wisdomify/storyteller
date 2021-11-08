3cee18871cf14c271ea5db787b993e7b6fa3c890
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-11-08 19:58:20 +0900

    #28 grammer checker added

--- storyteller/preprocess.py
+++ storyteller/preprocess.py
@@ -6,6 +6,7 @@ import pandas as pd
 from typing import Tuple
 from sklearn.model_selection import train_test_split
 from soynlp.normalizer import emoticon_normalize, only_text
+from hanspell import spell_checker
 
 
 def augment(df: pd.DataFrame) -> pd.DataFrame:
@@ -50,10 +51,10 @@ def normalise(df: pd.DataFrame) -> pd.DataFrame:
     # TODO: normalise grammar errors (py-hanspell)
 
     # =============== normalise emoticons and shorts =============== #
-    df['eg'] = df['eg'].apply(lambda r: re.sub('\.*!+', '!', r))    # (....)! match
-    df['eg'] = df['eg'].apply(lambda r: re.sub('\.*\?+', '?', r))   # (....)? match
-    df['eg'] = df['eg'].apply(lambda r: re.sub('\.+', '.', r))      # (....). match
-    df['eg'] = df['eg'].apply(lambda r: re.sub(',+', ',', r))       # (,,,,), match
+    df['eg'] = df['eg'].apply(lambda r: re.sub('\.*!+', '!', r))  # (....)! match
+    df['eg'] = df['eg'].apply(lambda r: re.sub('\.*\?+', '?', r))  # (....)? match
+    df['eg'] = df['eg'].apply(lambda r: re.sub('\.+', '.', r))  # (....). match
+    df['eg'] = df['eg'].apply(lambda r: re.sub(',+', ',', r))  # (,,,,), match
     # ㄱ-ㅎ이 따로 쓰일 경우를 대비해 ㄱ-ㅎ을 매칭시키게했었으나
     # ㅋ가 3번 사용한경우는 emoticon_normalise 에 걸리지 않아 자음 단독으로 쓰인 경우도 제거
     df['eg'] = df['eg'].apply(lambda r: re.sub('[^A-Za-z0-9가-힣\s\[\].,!?\"\']', '', r))
@@ -64,7 +65,10 @@ def normalise(df: pd.DataFrame) -> pd.DataFrame:
     df['eg'] = df['eg'].apply(lambda r: re.sub('\s+', ' ', r))  # multiple spacing match
 
     # ==================== grammar error check +=================== #
-    df['eg'] = df['eg'].apply(lambda r: re.sub('\s+', ' ', r))  # multiple spacing match
+    # py-hanspell을 설치해야하는 데 pip으로 설치하면 이전 버전의 pip으로 실행되서 에러가 발생한다.
+    # 반드시 해당 프로젝트의 리포를 클론후 storyteller 프로젝트 환경이 활성화되어 있는 상태에서 `python3 setup.py install`을 실행해야한다.
+    # 이 스텝이 가장 time consuming 한데 흠...
+    df['eg'] = df['eg'].apply(lambda r: spell_checker.check(r).checked)  # grammar check
 
     return df
 
